b2ce6ba3374489bf12f09f8b81fb1f6b799c226e
Daniel Foerster <pydsigner@gmail.com>
2020-11-14 19:53:17 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Co-regent
+
+A toolkit for building Python games, especially with [Kivy](https://kivy.org).
+
+Licensed under the [Apache License, 2.0](http://www.apache.org/licenses/LICENSE-2.0).
---
+++ setup.py
@@ -0,0 +1,40 @@
+import setuptools
+import re
+
+
+def read_description(path='README.md'):
+    with open(path) as f:
+        return f.read()
+
+
+def scrape_version(path='coregent/version.py'):
+    with open(path) as f:
+        text = f.read()
+
+    m = re.search(r'''__version__ = ['"](.*?)['"]''', text)
+    if m:
+        print(m, m.groups())
+        return m.group(1)
+
+
+setuptools.setup(
+    name='coregent',
+    version=scrape_version(),
+    author='Daniel Foerster',
+    author_email='pydsigner@gmail.com',
+    description='Co-regent: a toolkit for building Python games, especially with Kivy',
+    long_description=read_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/neurite-interactive/coregent',
+    packages=['coregent'],
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    python_requires='>=3.7',
+)
